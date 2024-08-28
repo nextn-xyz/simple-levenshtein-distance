3f6d5e447062bf993a8a3600a521fd30d403d1c2
MrTob <mr.tobtv@gmail.com>
2024-08-28 10:45:19 +0200

    ADD: init project

---
+++ src/main/java/xyz/nextn/levenshteindistance/LevenshteinDistance.java
@@ -0,0 +1,35 @@
+package xyz.nextn.levenshteindistance;
+/**
+ * Utility class for calculating the Levenshtein distance between two strings.
+ */
+public class LevenshteinDistance {
+
+    /**
+     * Calculates the Levenshtein distance between two strings.
+     *
+     * @param s1 The first string.
+     * @param s2 The second string.
+     * @return The Levenshtein distance between the two strings.
+     */
+    public static int calculate(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + (s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1),
+                            Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
---
+++ src/test/java/xyz/nextn/levenshteindistance/LevenshteinDistanceTest.java
@@ -0,0 +1,24 @@
+package xyz.nextn.levenshteindistance;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LevenshteinDistanceTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "'kitten', 'kitten', 0",
+            "'kitten', 'sitting', 3",
+            "'flaw', 'lawn', 2",
+            "'gumbo', 'gambol', 2",
+            "'book', 'back', 2",
+            "'', 'abc', 3",
+            "'abc', '', 3",
+            "'', '', 0",
+    })
+    void testCalculate(String s1, String s2, int expected) {
+        assertEquals(expected, LevenshteinDistance.calculate(s1, s2));
+    }
+}
