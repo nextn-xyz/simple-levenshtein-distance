b517d8c575f2a9e9bbc8352365ff8715ec886240
MrTob <mr.tobtv@gmail.com>
2024-08-28 22:21:04 +0200

    ADD: tests

--- src/test/java/xyz/nextn/levenshteindistance/LevenshteinDistanceTest.java
+++ src/test/java/xyz/nextn/levenshteindistance/LevenshteinDistanceTest.java
@@ -1,5 +1,6 @@
 package xyz.nextn.levenshteindistance;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,8 +19,40 @@ class LevenshteinDistanceTest {
             "'abc', '', 3",
             "'', '', 0",
             "'abc', 'abc', 0",
+            "'----', '__', 4",
     })
     void testCalculate(String s1, String s2, int expected) {
         assertEquals(expected, LevenshteinDistance.calculate(s1, s2));
     }
+    @Test
+    void testCalculateNull() {
+        assertThrows(NullPointerException.class, () -> LevenshteinDistance.calculate("null", null));
+        assertThrows(NullPointerException.class, () -> LevenshteinDistance.calculate(null, "null"));
+        assertThrows(NullPointerException.class, () -> LevenshteinDistance.calculate(null, null));
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "'🐱', '🐱', 0",
+            "'🐱', '🐶', 1",
+            "'🐱', '🐭', 1",
+            "'🐱', '🐰', 1",
+            "'🐱', '😬❌', 2",
+            "'╰(*°▽°*)╯','(┬┬﹏┬┬)', '7'",
+    })
+    void testCalculateEmoji(String s1, String s2, int expected) {
+        assertEquals(expected, LevenshteinDistance.calculate(s1, s2));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'©', '©', 0",
+            "'©', '®', 1",
+            "'©', '™', 1",
+            "'©', '€', 1",
+    })
+    void testCalculateUnicode(String s1, String s2, int expected) {
+        assertEquals(expected, LevenshteinDistance.calculate(s1, s2));
+    }
+
+
 }
