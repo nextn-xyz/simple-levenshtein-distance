90576f1457dba7480142ee3a7431bfebd9c886b5
MrTob <MrTob@users.noreply.github.com>
2024-08-28 08:45:43 +0000

    deploy: 3f6d5e447062bf993a8a3600a521fd30d403d1c2

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"xyz.nextn.levenshteindistance"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"xyz.nextn.levenshteindistance","l":"LevenshteinDistance"}];updateSearchResults();
